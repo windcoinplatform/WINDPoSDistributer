8ed12a25c747995741ccc3445ceae80c112c23d3
WIND <57537825+windcoinplatform@users.noreply.github.com>
2019-12-04 22:35:15 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+# WINDPoSDistributer
+
+#Leasing definitely do not have WIND !!!
+
+Rewards collected by the node within 24 hours are distributed as follows.
+
+1- Leasing addresses more than 1000 blocks are not included in the calculation.
+
+2- Transfer fee is deducted from the total balance in the account as much as the number of addresses to be distributed. (0.001 WIND * (number of wallets + 1))
+
+3- 5% of the remaining balance is transferred to the address (ceo_addr), 95% of which is proportional to the leasing amount of the leasing wallets.
+
+As a result of the distribution, the balance at leasing address remains close to zero.
+
+
+
+#The following files will be located in the same folder as the code to run.
+
+#crypto.py
+
+#wind_asset_default.py
+
+#windpy.py
+
+
+For manual deployment, use the WINDPoSDistributer_manuel program.
+
+Distribute manually for longer time periods.
---
+++ WINDPoSDistributer_manuel.py
@@ -0,0 +1,59 @@
+
+
+import requests
+import json
+import windpy as pw
+import time
+
+
+
+seedy = "SEED"
+myAddress = pw.Address(seed=seedy)
+
+   
+leasing_addr = "..................."
+ceo_addr = "............................"
+node = "http://144.91.84.27:6869"
+
+r = requests.get(node +"/leasing/active/" + leasing_addr)
+r = r.text
+r = json.loads(r)
+
+leasing_addr_balance =(json.loads((requests.get(node +"/addresses/balance/" + (leasing_addr))).text))['balance']
+
+
+block_height = json.loads((requests.get(node+"/blocks/height")).text)['height']
+
+
+
+if leasing_addr_balance >= 500000000:
+    sender = []
+    amount = []
+    height = []
+    n = 0
+    t_amount = 0
+    for i in r:
+        heightt = i['height']
+        if block_height - heightt > 1000:
+            sender =sender + [i['sender']]
+            amount =amount + [i['amount']]
+            height =height + [i['height']]
+            t_amount = t_amount + i['amount']
+            n = n + 1
+    transfer_total_fee = 100000 * (n+1)
+    
+    transfer_amount = ((leasing_addr_balance - transfer_total_fee) * 0.95)
+    
+    ceo_amount = leasing_addr_balance - transfer_amount
+    
+    for j in range(n):
+            
+        print(sender[j], ((amount[j] / t_amount)* transfer_amount))
+        myAddress.sendWind(recipient = pw.Address(sender[j]), amount = int(((amount[j] / t_amount)* transfer_amount)))
+            
+    print(ceo_addr, ceo_amount - transfer_total_fee)       
+    myAddress.sendWind(recipient = pw.Address(ceo_addr), amount =int(ceo_amount - transfer_total_fee))
+
+
+
+

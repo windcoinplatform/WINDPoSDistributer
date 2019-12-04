de97731bdb3e8604247cd931107a430ff4ccfd5b
WIND <57537825+windcoinplatform@users.noreply.github.com>
2019-12-04 22:39:27 +0300

    Update WINDPoSDistributer_manuel.py

--- WINDPoSDistributer_manuel.py
+++ WINDPoSDistributer_manuel.py
@@ -1,31 +1,22 @@
 
-
 import requests
 import json
 import windpy as pw
 import time
 
-
-
-seedy = "SEED"
-myAddress = pw.Address(seed=seedy)
-
-   
+#Fill in the fields
+#---------------------------------------------------------------------------
+seedy = "SEED" 
 leasing_addr = "..................."
 ceo_addr = "............................"
+#-----------------------------------------------------------------------------
 node = "http://144.91.84.27:6869"
-
+myAddress = pw.Address(seed=seedy) 
 r = requests.get(node +"/leasing/active/" + leasing_addr)
 r = r.text
 r = json.loads(r)
-
 leasing_addr_balance =(json.loads((requests.get(node +"/addresses/balance/" + (leasing_addr))).text))['balance']
-
-
 block_height = json.loads((requests.get(node+"/blocks/height")).text)['height']
-
-
-
 if leasing_addr_balance >= 500000000:
     sender = []
     amount = []
@@ -41,19 +32,10 @@ if leasing_addr_balance >= 500000000:
             t_amount = t_amount + i['amount']
             n = n + 1
     transfer_total_fee = 100000 * (n+1)
-    
     transfer_amount = ((leasing_addr_balance - transfer_total_fee) * 0.95)
-    
     ceo_amount = leasing_addr_balance - transfer_amount
-    
-    for j in range(n):
-            
+    for j in range(n):  
         print(sender[j], ((amount[j] / t_amount)* transfer_amount))
-        myAddress.sendWind(recipient = pw.Address(sender[j]), amount = int(((amount[j] / t_amount)* transfer_amount)))
-            
+        myAddress.sendWind(recipient = pw.Address(sender[j]), amount = int(((amount[j] / t_amount)* transfer_amount)))        
     print(ceo_addr, ceo_amount - transfer_total_fee)       
     myAddress.sendWind(recipient = pw.Address(ceo_addr), amount =int(ceo_amount - transfer_total_fee))
-
-
-
-
